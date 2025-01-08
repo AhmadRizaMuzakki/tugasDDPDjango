be7aed3a51104fa3392fa040e69eaf0c5a5909d2
Ahmad Riza Muzakki <ahmadmuzakki60@gmail.com>
2025-01-08 22:38:18 +0700

    feat: membuat fitur checkout

--- verdevalvet/views.py
+++ verdevalvet/views.py
@@ -6,7 +6,29 @@ def index(request):
     return render(request, 'index.html')
 
 def checkout(request):
-    return
+    # Mengumpulkan data dari form yang dikirimkan
+    nama = request.POST.get('nama')
+    tanggal = request.POST.get('tanggal')
+    hari = request.POST.get('hari')
+    paket = request.POST.get('paket')
+    
+    # Membuat dictionary untuk menyimpan data tiket
+    data_tiket = {
+        'nama': nama,
+        'tanggal': tanggal,
+        'hari': hari,
+        'paket': paket,
+    }
+    
+    # Menyimpan data tiket ke dalam session
+    request.session['data_tiket'] = data_tiket
+    
+    # Mengambil data tiket dari session untuk digunakan dalam template
+    InfoTiket = request.session.get('data_tiket', {})
+    
+    # Mengembalikan render dengan data tiket yang telah diambil
+    return render(request, 'Tiket.html', {'InfoTiket': InfoTiket})
+
 def feedback(request):
     return
 def my_ticket(request):
