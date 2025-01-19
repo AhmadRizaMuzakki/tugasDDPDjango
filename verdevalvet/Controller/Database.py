9b6701a2f52a8117e3a51c5b76824334dbaf1698
Ahmad Riza Muzakki <ahmadmuzakki60@gmail.com>
2025-01-19 08:23:23 +0700

    feat: sudah bisa membuat tiket

--- verdevalvet/Controller/Database.py
+++ verdevalvet/Controller/Database.py
@@ -1,20 +1,46 @@
 
 
 class Database:
-    
-    def DataEvent(request):
-        tbl_event = [
-            {'id': 1, 'nama': 'Ramadhan Berbahagia', 'deskripsi': 'Acara ini akan menghadirkan berbagai penampilan musik dan tarian yang akan membuat Anda berbahagia.', 'time': '15 juni 2025', 'tempat': 'Gelora Bung Karno', 'poster': 'https://png.pngtree.com/thumb_back/fw800/background/20230321/pngtree-stage-concert-background-gold-image_2050942.jpg'},
-            {'id': 2, 'nama': 'Akbar Gebyar', 'deskripsi': 'Akbar Gebyar adalah acara yang paling ditunggu-tunggu di tahun ini. Dengan penampilan musik yang sangat beragam, Anda tidak akan kecewa.', 'time': '20 juli 2025', 'tempat': 'Stadion Utama Gelora Bung Karno', 'poster': 'https://png.pngtree.com/background/20210715/original/pngtree-creative-carnival-music-festival-hd-background-picture-image_1318639.jpg'},
-            {'id': 3, 'nama': 'Senayan Berpesta', 'deskripsi': 'Senayan Berpesta adalah acara yang akan menghadirkan berbagai penampilan musik dan tarian yang sangat beragam dan menarik.', 'time': '25 agustus 2025', 'tempat': 'Lapangan D Senayan', 'poster': 'https://1.bp.blogspot.com/-4JBNfe_H6PQ/YD5giaQTVGI/AAAAAAAABoA/PpWIew4qnQgHvvv0NlaDM5ZU017UPrNKACLcBGAsYHQ/s1200/konser.jpg'},
-            {'id': 4, 'nama': 'Jakarta Berpesta', 'deskripsi': 'Jakarta Berpesta adalah acara yang paling ditunggu-tunggu di Jakarta. Dengan penampilan musik yang sangat beragam, Anda tidak akan kecewa.', 'time': '10 september 2025', 'tempat': 'Taman Mini Indonesia Indah', 'poster': 'https://img.freepik.com/premium-vector/music-event-banner-design-template-festival-concert-party_85212-64.jpg?w=1380'},
-            {'id': 5, 'nama': 'Festival Musik Jakarta', 'deskripsi': 'Festival Musik Jakarta adalah acara yang akan menghadirkan berbagai penampilan musik dari berbagai genre.', 'time': '15 oktober 2025', 'tempat': 'Gelora Bung Karno', 'poster': 'https://png.pngtree.com/thumb_back/fw800/background/20230321/pngtree-stage-concert-background-gold-image_2050942.jpg'},
-            {'id': 6, 'nama': 'Konser Amal', 'deskripsi': 'Konser Amal adalah acara yang diadakan untuk mengumpulkan dana bagi korban bencana alam.', 'time': '20 november 2025', 'tempat': 'Stadion Utama Gelora Bung Karno', 'poster': 'https://png.pngtree.com/background/20210715/original/pngtree-creative-carnival-music-festival-hd-background-picture-image_1318639.jpg'},
-            {'id': 7, 'nama': 'Festival Budaya', 'deskripsi': 'Festival Budaya adalah acara yang akan menghadirkan berbagai penampilan budaya dari berbagai daerah di Indonesia.', 'time': '25 desember 2025', 'tempat': 'Lapangan D Senayan', 'poster': 'https://1.bp.blogspot.com/-4JBNfe_H6PQ/YD5giaQTVGI/AAAAAAAABoA/PpWIew4qnQgHvvv0NlaDM5ZU017UPrNKACLcBGAsYHQ/s1200/konser.jpg'},
-            {'id': 8, 'nama': 'Pesta Rakyat', 'deskripsi': 'Pesta Rakyat adalah acara yang diadakan untuk memperingati hari kemerdekaan Indonesia.', 'time': '17 agustus 2025', 'tempat': 'Taman Mini Indonesia Indah', 'poster': 'https://img.freepik.com/premium-vector/music-event-banner-design-template-festival-concert-party_85212-64.jpg?w=1380'},
-            {'id': 9, 'nama': 'Festival Film', 'deskripsi': 'Festival Film adalah acara yang akan menghadirkan berbagai film dari berbagai genre.', 'time': '10 januari 2026', 'tempat': 'Gelora Bung Karno', 'poster': 'https://png.pngtree.com/thumb_back/fw800/background/20230321/pngtree-stage-concert-background-gold-image_2050942.jpg'},
-            {'id': 10, 'nama': 'Pesta Musik', 'deskripsi': 'Pesta Musik adalah acara yang akan menghadirkan berbagai penampilan musik dari berbagai genre.', 'time': '15 februari 2026', 'tempat': 'Stadion Utama Gelora Bung Karno', 'poster': 'https://png.pngtree.com/background/20210715/original/pngtree-creative-carnival-music-festival-hd-background-picture-image_1318639.jpg'},
+    def SesiKonser(request):
+        sesi_konser = [
+            {'id': 1, 'nama': 'Sesi Siang', 'waktu': '13:00 - 16:00' },
+            {'id': 2, 'nama': 'Sesi Malam', 'waktu': '19:00 - 22:00' },
+            {'id': 3, 'nama': 'Sesi Siang', 'waktu': '13:00 - 16:00' },
+            {'id': 4, 'nama': 'Sesi Malam', 'waktu': '19:00 - 22:00'},
+            {'id': 5, 'nama': 'Sesi Siang', 'waktu': '13:00 - 16:00'},
+            {'id': 6, 'nama': 'Sesi Malam', 'waktu': '19:00 - 22:00'},
         ]
+        return sesi_konser
+    def DataKursi(request):
+        nomor_kursi = [
+            {'id': 1, 'nomor': ['A1', 'A2', 'A3', 'A4', 'A5'], 'kategori': 'VIP', 'status': 'tersedia'},
+            {'id': 2, 'nomor': ['B1', 'B2', 'B3', 'B4', 'B5'], 'kategori': 'Premium', 'status': 'tersedia'},
+            {'id': 3, 'nomor': ['C1', 'C2', 'C3', 'C4', 'C5'], 'kategori': 'Basic', 'status': 'tersedia'},
+        ]
+        return nomor_kursi
+    
+    def DataPaket(request):
+        tbl_paket = [
+            {'id': 1, 'grade': 'vip', 'harga': 3000000},
+            {'id': 2, 'grade': 'premium', 'harga': 1000000},
+            {'id': 3, 'grade': 'basic', 'harga': 700000},
+        ]
+        return tbl_paket
+    def DataEvent(request):
+        
+        tbl_event =  [
+            {'id': 1, 'nama': 'Ramadhan Berbahagia', 'deskripsi': 'Acara ini akan menghadirkan berbagai penampilan musik dan tarian yang akan membuat Anda berbahagia.', 'time': '15 juni 2025', 'bangunan': 'Gelora Bung Karno', 'alamat': 'Jalan Pintu 1 Senayan, Gelora, Tanah Abang, Jakarta Pusat, DKI Jakarta 10270', 'poster': 'https://png.pngtree.com/thumb_back/fw800/background/20230321/pngtree-stage-concert-background-gold-image_2050942.jpg', 'artis': ['Tulus', 'Agnez Mo', 'Isyana Sarasvati']},
+            {'id': 2, 'nama': 'Akbar Gebyar', 'deskripsi': 'Akbar Gebyar adalah acara yang paling ditunggu-tunggu di tahun ini. Dengan penampilan musik yang sangat beragam, Anda tidak akan kecewa.', 'time': '20 juli 2025', 'bangunan': 'Stadion Utama Gelora Bung Karno', 'alamat': 'Jalan Pintu 1 Senayan, Gelora, Tanah Abang, Jakarta Pusat, DKI Jakarta 10270', 'poster': 'https://png.pngtree.com/background/20210715/original/pngtree-creative-carnival-music-festival-hd-background-picture-image_1318639.jpg', 'artis': ['Raisa', 'Judika', 'Maliq & D’Essentials']},
+            {'id': 3, 'nama': 'Senayan Berpesta', 'deskripsi': 'Senayan Berpesta adalah acara yang akan menghadirkan berbagai penampilan musik dan tarian yang sangat beragam dan menarik.', 'time': '25 agustus 2025', 'bangunan': 'Lapangan D Senayan', 'alamat': 'Jalan Jenderal Sudirman, Senayan, Kebayoran Baru, Jakarta Selatan, DKI Jakarta 12190', 'poster': 'https://1.bp.blogspot.com/-4JBNfe_H6PQ/YD5giaQTVGI/AAAAAAAABoA/PpWIew4qnQgHvvv0NlaDM5ZU017UPrNKACLcBGAsYHQ/s1200/konser.jpg', 'artis': ['Ariel NOAH', 'Bunga Citra Lestari', 'Gigi']},
+            {'id': 4, 'nama': 'Jakarta Berpesta', 'deskripsi': 'Jakarta Berpesta adalah acara yang paling ditunggu-tunggu di Jakarta. Dengan penampilan musik yang sangat beragam, Anda tidak akan kecewa.', 'time': '10 september 2025', 'bangunan': 'Taman Mini Indonesia Indah', 'alamat': 'Jalan Raya Taman Mini Indonesia Indah, Pinang Ranti, Makasar, Jakarta Timur, DKI Jakarta 13560', 'poster': 'https://img.freepik.com/premium-vector/music-event-banner-design-template-festival-concert-party_85212-64.jpg?w=1380', 'artis': ['Yura Yunita', 'Danilla Riyadi', 'Padi Reborn']},
+            {'id': 5, 'nama': 'Festival Musik Jakarta', 'deskripsi': 'Festival Musik Jakarta adalah acara yang akan menghadirkan berbagai penampilan musik dari berbagai genre.', 'time': '15 oktober 2025', 'bangunan': 'Gelora Bung Karno', 'alamat': 'Jalan Pintu 1 Senayan, Gelora, Tanah Abang, Jakarta Pusat, DKI Jakarta 10270', 'poster': 'https://png.pngtree.com/thumb_back/fw800/background/20230321/pngtree-stage-concert-background-gold-image_2050942.jpg', 'artis': ['Noah', 'Tulus', 'Ari Lasso']},
+            {'id': 6, 'nama': 'Konser Amal', 'deskripsi': 'Konser Amal adalah acara yang diadakan untuk mengumpulkan dana bagi korban bencana alam.', 'time': '20 november 2025', 'bangunan': 'Stadion Utama Gelora Bung Karno', 'alamat': 'Jalan Pintu 1 Senayan, Gelora, Tanah Abang, Jakarta Pusat, DKI Jakarta 10270', 'poster': 'https://png.pngtree.com/background/20210715/original/pngtree-creative-carnival-music-festival-hd-background-picture-image_1318639.jpg', 'artis': ['Agnez Mo', 'Raisa', 'Andien']},
+            {'id': 7, 'nama': 'Festival Budaya', 'deskripsi': 'Festival Budaya adalah acara yang akan menghadirkan berbagai penampilan budaya dari berbagai daerah di Indonesia.', 'time': '25 desember 2025', 'bangunan': 'Lapangan D Senayan', 'alamat': 'Jalan Jenderal Sudirman, Senayan, Kebayoran Baru, Jakarta Selatan, DKI Jakarta 12190', 'poster': 'https://1.bp.blogspot.com/-4JBNfe_H6PQ/YD5giaQTVGI/AAAAAAAABoA/PpWIew4qnQgHvvv0NlaDM5ZU017UPrNKACLcBGAsYHQ/s1200/konser.jpg', 'artis': ['Batik Keris', 'Kerispatih', 'Slank']},
+            {'id': 8, 'nama': 'Pesta Rakyat', 'deskripsi': 'Pesta Rakyat adalah acara yang diadakan untuk memperingati hari kemerdekaan Indonesia.', 'time': '17 agustus 2025', 'bangunan': 'Taman Mini Indonesia Indah', 'alamat': 'Jalan Raya Taman Mini Indonesia Indah, Pinang Ranti, Makasar, Jakarta Timur, DKI Jakarta 13560', 'poster': 'https://img.freepik.com/premium-vector/music-event-banner-design-template-festival-concert-party_85212-64.jpg?w=1380', 'artis': ['Gigi', 'Iwan Fals', 'The Titans']},
+            {'id': 9, 'nama': 'Festival Film', 'deskripsi': 'Festival Film adalah acara yang akan menghadirkan berbagai film dari berbagai genre.', 'time': '10 januari 2026', 'bangunan': 'Gelora Bung Karno', 'alamat': 'Jalan Pintu 1 Senayan, Gelora, Tanah Abang, Jakarta Pusat, DKI Jakarta 10270', 'poster': 'https://png.pngtree.com/thumb_back/fw800/background/20230321/pngtree-stage-concert-background-gold-image_2050942.jpg', 'artis': ['Vino G. Bastian', 'Reza Rahadian', 'Titi Kamal']},
+            {'id': 10, 'nama': 'Pesta Musik', 'deskripsi': 'Pesta Musik adalah acara yang akan menghadirkan berbagai penampilan musik dari berbagai genre.', 'time': '15 februari 2026', 'bangunan': 'Stadion Utama Gelora Bung Karno', 'alamat': 'Jalan Pintu 1 Senayan, Gelora, Tanah Abang, Jakarta Pusat, DKI Jakarta 10270', 'poster': 'https://png.pngtree.com/background/20210715/original/pngtree-creative-carnival-music-festival-hd-background-picture-image_1318639.jpg', 'artis': ['Rizky Febian', 'Isyana Sarasvati', 'Vidi Aldiano']},
+        ]
+
         return tbl_event
     def DataArtis(request):
         tbl_artis = [
